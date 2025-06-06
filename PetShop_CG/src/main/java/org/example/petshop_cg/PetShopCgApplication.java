7bb511df749e7945bdd8ad4630b7ba06a391b694
RAHULPATEL-2017 <rahul2017.be21@chitkara.edu.in>
2025-06-06 11:15:22 +0530

    [RahulPatel] add : PetShop_CG

---
+++ PetShop_CG/src/main/java/org/example/petshop_cg/PetShopCgApplication.java
@@ -0,0 +1,13 @@
+package org.example.petshop_cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShopCgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetShopCgApplication.class, args);
+    }
+
+}
